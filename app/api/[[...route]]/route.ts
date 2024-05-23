f0aa1ba93e854edd60a5cc2e47494282bcc9cbee
luhan <luhan@360.cn>
2024-05-23 17:22:21 +0800

    feat: add route

---
+++ app/api/[[...route]]/route.ts
@@ -0,0 +1,20 @@
+import { z } from 'zod';
+import { Hono } from 'hono'
+import { handle } from 'hono/vercel'
+import { zValidator } from '@hono/zod-validator';
+import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
+
+import authors from './authors'
+import books from './books'
+
+export const runtime = 'edge';
+
+const app = new Hono().basePath('/api')
+
+app.route('/authors', authors)
+app.route('/books', books)
+
+app.use('*', clerkMiddleware())
+
+export const GET = handle(app)
+export const POST = handle(app)
