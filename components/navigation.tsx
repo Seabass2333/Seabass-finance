8d9fbceadecfc88b2d77c31234e31df1c96fb4ea
Seabass <soft_world@163.com>
2024-05-29 09:17:46 +0800

    feat：add sheet

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,7 +1,13 @@
 'use client'
 
-import { usePathname } from 'next/navigation'
+import { useState } from 'react'
+import { useMedia } from 'react-use'
+import { usePathname, useRouter } from 'next/navigation'
+
 import NavButton from '@/components/nav-button'
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
+import { Button } from '@/components/ui/button'
+import { Menu } from 'lucide-react'
 
 const routes = [
   {
@@ -27,7 +33,47 @@ const routes = [
 ]
 
 const Navigation = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const router = useRouter()
   const pathname = usePathname()
+  const isMobile = useMedia('(max-width: 1023px)', true)
+
+  const onClick = (href: string) => {
+    router.push(href)
+    setIsOpen(false)
+  }
+
+  if (isMobile) {
+    return (
+      <Sheet
+        open={isOpen}
+        onOpenChange={setIsOpen}
+      >
+        <SheetTrigger className='font-normal text-white p-3 rounded-sm bg-white/10 hover:bg-white/20 focus:bg-white/30 transition'>
+          <Menu className='size-4' />
+        </SheetTrigger>
+        <SheetContent
+          side='left'
+          className='px-2'
+        >
+          <nav className='flex flex-col gap-y-2 pt-6'>
+            {routes.map((route) => (
+              <Button
+                key={route.path}
+                variant={route.path === pathname ? 'secondary' : 'ghost'}
+                onClick={() => onClick(route.path)}
+                className='w-full justify-start'
+              >
+                {route.component}
+              </Button>
+            ))}
+          </nav>
+        </SheetContent>
+      </Sheet>
+    )
+  }
+
   return (
     <nav className='hidden lg:flex items-center gap-2 overflow-x-auto'>
       {routes.map((route, index) => (
